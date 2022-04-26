d47dfea676eca617b3801ddab558318a97f18131
antonio <antonio.m.palma@gmail.com>
2022-04-26 19:11:04 +0100

    Concluido

--- models/LinkedList.py
+++ models/LinkedList.py
@@ -107,7 +107,7 @@ class LinkedList:
         n = self.start_node
         while n is not None:
             if n.item == x:
-                print("Item found")
+                print("item found")
                 return True
             n = n.ref
         print("item not found")
