c151b27e1e45dd859666ab8757f42511d168c453
antonio <antonio.m.palma@gmail.com>
2022-04-26 19:32:31 +0100

    Concluido

--- program.py
+++ program.py
@@ -6,3 +6,6 @@ sys.stdout.reconfigure(encoding='utf-8')
 if __name__ == '__main__':
     view.main()
     
+#Este programa percorre uma lista-ligada de paises, possui 10 comandos que, realizam
+#estas respetivas ações. Permite um flexibilidade grande do modo como é feito o registo do
+#país, quer seja no início da lista, ou verificar a existência de um país.
